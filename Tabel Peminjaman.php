9a1b07e626f684fd135efffd2c9aa98ea1224fa9
jrcodingx <abangjrgeminkk@gmail.com>
2025-05-23 11:07:52 +0700

    Add files via upload

---
+++ Tabel Peminjaman.php
@@ -0,0 +1,73 @@
+<?php
+// Konfigurasi koneksi database
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "db_perpustakaan";
+$conn = new mysqli($host, $user, $pass, $db);
+// Cek koneksi
+if ($conn->connect_error) {
+ die("Koneksi gagal: " . $conn->connect_error);
+}
+// Ambil data dari tabel
+$sql = "SELECT * FROM tbl_peminjaman ORDER BY Kode_Peminjaman DESC";
+$result = $conn->query($sql);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Data Peminjaman</title>
+ <style>
+ table {
+ border-collapse: collapse;
+ width: 100%;
+ }
+ table, td, th {
+ border: 1px solid #000;
+ padding: 8px;
+ }
+ th {
+ background-color: #eee;
+ }
+ </style>
+</head>
+<body>
+ <h2>Daftar Peminjaman</h2>
+ <a href="add.php">+ Tambah Peminjaman Baru</a>
+ <br><br>
+ <table>
+ <tr>
+ <th>Kode Peminjaman</th>
+ <th>ID Buku</th>
+ <th>Nama Anggota</th>
+ <th>Judul Buku</th>
+ <th>Tanggal Peminjaman</th>
+ <th>Status</th>
+ </tr>
+ <?php
+ if ($result->num_rows > 0) {
+ $no = 1;
+ while($row = $result->fetch_assoc()) {
+ echo "<tr>
+ <td>".$no++."</td>
+<td>".$row['Kode_Peminjaman']."</td>
+<td>".$row['id_buku']."</td>
+<td>".$row['id_anggota']."</td>
+<td>".$row['nama']."</td>
+<td>".$row['judul']."</td>
+<td>".$row['status']."</td>
+<td>
+ <a
+href='edit.php?id=".$row['Kode_Peminjaman']."'>Edit</a> |
+ <a href='delete.php?id=".$row['Kode_Peminjaman']."'
+onclick=\"return confirm('Yakin ingin menghapus?')\">Hapus</a>
+ </td>
+ </tr>";
+ }
+ } else {
+ echo "<tr><td colspan='7'>Belum ada peminjaman.</td></tr>";
+ }
+ ?>
+ </table>
+</body>
+</html>

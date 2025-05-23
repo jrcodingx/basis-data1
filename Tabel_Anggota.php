ee6d3ad582ddf9278f99b0d5070ba8d952e2ccf7
jrcodingx <abangjrgeminkk@gmail.com>
2025-05-23 17:21:52 +0700

    Update Tabel_Anggota.php

--- Tabel_Anggota.php
+++ Tabel_Anggota.php
@@ -64,7 +64,7 @@ onclick=\"return confirm('Yakin ingin menghapus?')\">Hapus</a>
         } 
         ?> 
     </table>
-    <a href="./">Kembali ke Dashboard</a>
+    <a href="./">Kembali</a>
     <br><br> 
 </body> 
 </html>

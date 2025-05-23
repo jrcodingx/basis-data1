2a770b880117afe6f7301b4dce306cd7dc484544
jrcodingx <abangjrgeminkk@gmail.com>
2025-05-23 11:17:08 +0700

    Update Tabel Peminjaman.php

--- Tabel Peminjaman.php
+++ Tabel Peminjaman.php
@@ -1,10 +1,9 @@
 <?php
 // Konfigurasi koneksi database
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "db_perpustakaan";
-$conn = new mysqli($host, $user, $pass, $db);
+$host = "localhost"; 
+$user = "perpus";
+$pass = "passwordku123";
+$db   = "db_perpustakaan";
 // Cek koneksi
 if ($conn->connect_error) {
  die("Koneksi gagal: " . $conn->connect_error);

91560e0531d7e761fdb3e3c90089af948d1ac52a
dingdingding <abangjrgeminkk@gmail.com>
2025-05-26 09:39:55 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,15 +1,18 @@
 <?php
-// Konfigurasi koneksi database
-$host = "localhost"; 
-$user = "perpus";
-$pass = "passwordku123";
-$db   = "db_perpustakaan";
+// Konfigurasi koneksi database PostgreSQL
+$host = "localhost";
+$port = "5432";
+$dbname = "db_perpustakaan_pg";
+$user = "perpus_pg";
+$password = "passwordku123";
 
 // Membuat koneksi
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
 // Cek koneksi
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
+if (!$conn) {
+    die("Koneksi ke PostgreSQL gagal: " . pg_last_error());
+} else {
+    echo "Koneksi berhasil!";
 }
 ?>

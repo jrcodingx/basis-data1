82d1203cee44ace6e01a3ced42386426391f4e4e
dingdingding <abangjrgeminkk@gmail.com>
2025-05-26 20:31:54 +0700

    Update A.php

--- A.php
+++ A.php
@@ -1,16 +1,16 @@
 <?php 
 // Konfigurasi koneksi database 
 include 'config.php';
- 
+
 // Ambil data dari tabel 
-$sql = "SELECT * FROM tbl_buku ORDER BY id_buku DESC"; 
+$sql = "SELECT * FROM tbl_anggota ORDER BY id_anggota DESC"; 
 $result = pg_query($conn, $sql); 
 ?> 
- 
+
 <!DOCTYPE html> 
 <html> 
 <head> 
-    <title>Data Buku Perpustakaan</title> 
+    <title>Data Anggota Perpustakaan</title> 
     <style> 
         table { 
             border-collapse: collapse; 
@@ -26,46 +26,43 @@ $result = pg_query($conn, $sql);
     </style> 
 </head> 
 <body> 
-    <h2>Daftar Buku Perpustakaan</h2> 
-    <a href="addbuku.php?tabel=tbl_buku">+ Tambah Buku Baru</a> 
+    <h2>Daftar Anggota Perpustakaan</h2> 
+    <a href="add.php">+ Tambah Anggota Baru</a> 
     <br><br> 
  
     <table> 
         <tr> 
             <th>No</th> 
-            <th>Isbn</th> 
-            <th>Judul</th> 
-            <th>Pengarang</th> 
-            <th>Penerbit</th> 
-            <th>Tahun Terbit</th> 
-            <th>Kategori</th> 
-            <th>Jumlah Buku</th> 
+            <th>NIM</th> 
+            <th>Nama</th> 
+            <th>Program Studi</th> 
+            <th>Tanggal Daftar</th> 
+            <th>Status</th> 
             <th>Aksi</th> 
         </tr> 
         <?php 
         if ($result && pg_num_rows($result) > 0) { 
             $no = 1; 
-            while($row = pg_fetch_assoc($result)) { 
+            while ($row = pg_fetch_assoc($result)) { 
                 echo "<tr> 
                         <td>".$no++."</td> 
-                        <td>".$row['isbn']."</td> 
-                        <td>".$row['judul']."</td> 
-                        <td>".$row['pengarang']."</td> 
-                        <td>".$row['penerbit']."</td> 
-                        <td>".$row['tahun_terbit']."</td> 
-                        <td>".$row['kategori']."</td> 
-                        <td>".$row['jumlah_buku']."</td> 
+                        <td>".$row['nim']."</td> 
+                        <td>".$row['nama']."</td> 
+                        <td>".$row['prodi']."</td> 
+                        <td>".$row['tgl_daftar']."</td> 
+                        <td>".$row['status']."</td> 
                         <td> 
-                            <a href='editbuku.php?id=".$row['id_buku']."'>Edit</a> |  
-                            <a href='deletebuku.php?id=".$row['id_buku']."' onclick=\"return confirm('Yakin ingin menghapus?')\">Hapus</a> 
+                            <a href='edit.php?id=".$row['id_anggota']."'>Edit</a> |  
+                            <a href='delete.php?id=".$row['id_anggota']."' onclick=\"return confirm('Yakin ingin menghapus?')\">Hapus</a> 
                         </td> 
                       </tr>"; 
             } 
         } else { 
-            echo "<tr><td colspan='9'>Belum ada data buku.</td></tr>"; 
+            echo "<tr><td colspan='7'>Belum ada data anggota.</td></tr>"; 
         } 
         ?> 
-    </table> 
+    </table>
     <a href="./">Kembali</a>
+    <br><br> 
 </body> 
 </html>
